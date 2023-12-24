c9fb1b4b06e7ba906fedd288266c73641a12ac4d
Kirill <troninkirill@gmail.com>
2023-12-24 08:41:30 +0700

    add prisma studio pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,13 @@ module.exports = {
     apps: [{
         name: "catsbot",
         script: "./build/src/index.js",
-        args: "--clean=false",
         error_file: "catsbot-err.log",
         out_file: "catsbot-out.log"
+    }, {
+        name: "db",
+        script: "npm",
+        args: "run db",
+        error_file: "catsbot-dberr.log",
+        out_file: "catsbot-db.log"
     }]
 }
